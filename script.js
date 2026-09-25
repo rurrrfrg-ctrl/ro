cab01bd1f5db87c1e17de1d4a6e7c035b76a0b78
rurrrfrg-ctrl <ruslana2008gr@gmail.com>
2026-09-25 16:11:00 +0300

    Rename js.txt to script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title>RO — Refined Objects</title>
+  <title>RO â€” Refined Objects</title>
 
   <link rel="stylesheet" href="style.css">
 </head>
